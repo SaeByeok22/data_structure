0be000eea493c04d19d08ac1c083162a23609cf1
SaeByeok22 <icon0615@kyonggi.ac.kr>
2026-04-07 10:56:09 +0900

    bt 4 ~ 7번 완료

--- Data-Structures/Binary_Tree/Q4_E_BT.c
+++ Data-Structures/Binary_Tree/Q4_E_BT.c
@@ -104,6 +104,15 @@ int sumOfOddNodes(BTNode *node)
 
 {
     /* add your code here */
+    if (node == NULL) {
+        return 0;
+    }
+
+    if (node->item % 2 != 0) {
+        return node->item + sumOfOddNodes(node->left) + sumOfOddNodes(node->right);
+    } else {
+        return sumOfOddNodes(node->left) + sumOfOddNodes(node->right);
+    }
 }
 
 //////////////////////////////////////////////////////////////////////////////////
